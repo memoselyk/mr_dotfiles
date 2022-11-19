dbde4b69af305d5fd29deca082747695fe8407ca
TJ DeVries <devries.timothyj@gmail.com>
2022-11-18 21:04:04 -0500

    [plugin][ks]Add figet: lsp status indicator

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -144,9 +144,14 @@ require('lazy').setup {
   { -- LSP Configuration & Plugins
     'neovim/nvim-lspconfig',
     dependencies = {
+      -- Automatically install LSPs and related tools to stdpath for neovim
       'williamboman/mason.nvim',
       'williamboman/mason-lspconfig.nvim',
       'WhoIsSethDaniel/mason-tool-installer.nvim',
+
+      -- Useful status updates for LSP.
+      -- NOTE: `opts = {}` is the same as calling `require('fidget').setup({})`
+      { 'j-hui/fidget.nvim', opts = {} },
     },
     config = function()
       -- Brief Aside: **What is LSP?**
