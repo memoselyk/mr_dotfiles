fd741964d3c6b905b59adcf4ade79f8960653dac
Charlie <charliesbot@gmail.com>
2023-01-12 23:09:09 -0800

    Add LSP keymaps from charliesbot
    From: https://youtu.be/Y2iqRZ4EYbk

--- .config/nvim/after/plugin/lsp.lua
+++ .config/nvim/after/plugin/lsp.lua
@@ -15,6 +15,22 @@ lsp.ensure_installed({
   'lua_ls',
 })
 
+lsp.on_attach(function(client, bufnr)
+  local opts = { buffer = bufnr, remap = false }
+
+  -- Mapping defined by charliesbot
+  vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
+  vim.keymap.set('n', 'gh', vim.lsp.buf.hover, opts)
+  vim.keymap.set('n', '<leader>vws', vim.lsp.buf.workspace_symbol, opts)
+  vim.keymap.set('n', '<leader>vd', vim.diagnostic.open_float, opts)
+  vim.keymap.set('n', '<leader>n', vim.diagnostic.goto_next, opts)
+  vim.keymap.set('n', '<leader>m', vim.diagnostic.goto_prev, opts)
+  vim.keymap.set('n', '<leader>vca', vim.lsp.buf.code_action, opts)
+  vim.keymap.set('n', '<leader>vrr', vim.lsp.buf.references, opts)
+  vim.keymap.set('n', '<leader>vrn', vim.lsp.buf.rename, opts)
+  vim.keymap.set('n', '<leader>sh', vim.lsp.buf.signature_help, opts)
+end)
+
 -- (Optional) Configure lua language server for neovim
 lsp.nvim_workspace()
 
