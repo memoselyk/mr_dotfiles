646b2b72f873925b1e73ca44943c3d98cc130b21
memoselyk <memoselyk@gmail.com>
2023-03-12 19:12:17 +0000

    Add LSP with lua support

---
+++ .config/nvim/after/plugin/lsp.lua
@@ -0,0 +1,19 @@
+local lsp = require('lsp-zero')
+lsp.preset('recommended')
+
+-- Configuration from README
+-- lsp.preset({
+--   name = 'minimal',
+--   set_lsp_keymaps = true,
+--   manage_nvim_cmp = true,
+--   suggest_lsp_servers = false,
+-- })
+
+lsp.ensure_installed({
+  'lua_ls',
+})
+
+-- (Optional) Configure lua language server for neovim
+lsp.nvim_workspace()
+
+lsp.setup()
