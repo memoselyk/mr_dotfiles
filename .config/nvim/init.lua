6bf4d22430cd81cface7e3875be1fc1742c6fc28
memoselyk <memoselyk@gmail.com>
2024-03-10 04:29:55 +0000

    Convert lsp server config to kickstart style

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -108,12 +108,23 @@ require('lazy').setup {
       'williamboman/mason-lspconfig.nvim',
     },
     config = function()
-      local lspconfig = require 'lspconfig'
-      lspconfig.lua_ls.setup {}
       vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
       vim.keymap.set('n', '<leader>gd', vim.lsp.buf.definition, {})
       -- vim.keymap.set('n', '<leader>gr', vim.lsp.buf.references, {})
       vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, {})
+
+      -- Enable the following language servers
+      --  Feel free to add/remove any LSPs that you want here. They will automatically be installed.
+      --
+      --  Add any additional override configuration in the following tables. Available keys are:
+      --  - cmd (table): Override the default command used to start the server
+      --  - filetypes (table): Override the default list of associated filetypes for the server
+      --  - capabilities (table): Override fields in capabilities. Can be used to disable certain LSP features.
+      --  - settings (table): Override the default settings passed when initializing the server.
+      --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
+      local servers = {
+        lua_ls = {},
+      }
 
       -- Ensure the servers and tools above are installed
       --  To check the current status of installed tools and/or manually install
@@ -123,8 +134,18 @@ require('lazy').setup {
       --  You can press `g?` for help in this menu
       require('mason').setup()
 
+      -- You can add other tools here that you want Mason to install
+      -- for you, so that they are available from within Neovim.
+      local ensure_installed = vim.tbl_keys(servers or {})
+
       require('mason-lspconfig').setup {
-        ensure_installed = { 'lua_ls' },
+        ensure_installed = ensure_installed,
+        handlers = {
+          function(server_name)
+            local server = servers[server_name] or {}
+            require('lspconfig')[server_name].setup(server)
+          end,
+        },
       }
     end,
   },
