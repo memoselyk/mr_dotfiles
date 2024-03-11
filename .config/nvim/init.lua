b26678aa3bb1c45a87680e5eb25af7bc58e19557
memoselyk <memoselyk@gmail.com>
2024-03-11 02:50:08 +0000

    Add todo-comments plugin

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -323,6 +323,14 @@ require('lazy').setup {
     end,
   },
 
+  -- Highlight todo, notes, etc in comments
+  {
+    'folke/todo-comments.nvim',
+    event = 'VimEnter',
+    dependencies = { 'nvim-lua/plenary.nvim' },
+    opts = { signs = false },
+  },
+
   { -- Highlight, edit, and navigate code
     'nvim-treesitter/nvim-treesitter',
     build = ':TSUpdate',
