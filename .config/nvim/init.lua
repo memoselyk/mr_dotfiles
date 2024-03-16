bfd45c40ad017a95e60b2e5142486c9b4cb46492
memoselyk <memoselyk@gmail.com>
2024-03-16 18:28:55 +0000

    [ks] Cosmetic align treesitter configuration

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -511,14 +511,22 @@ require('lazy').setup {
   { -- Highlight, edit, and navigate code
     'nvim-treesitter/nvim-treesitter',
     build = ':TSUpdate',
-    config = function()
+    opts = {
+      ensure_installed = { 'bash', 'lua', 'markdown', 'vim', 'vimdoc' },
+      highlight = { enable = true },
+      indent = { enable = true },
+    },
+    config = function(_, opts)
       -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
 
-      require('nvim-treesitter.configs').setup {
-        ensure_installed = { 'bash', 'lua', 'markdown', 'vim', 'vimdoc' },
-        highlight = { enable = true },
-        indent = { enable = true },
-      }
+      require('nvim-treesitter.configs').setup(opts)
+
+      -- There are additional nvim-treesitter modules that you can use to interact
+      -- with nvim-treesitter. You should go explore a few and see what interests you:
+      --
+      --    - Incremental selection: Included, see `:help nvim-treesitter-incremental-selection-mod`
+      --    - Show your current context: https://github.com/nvim-treesitter/nvim-treesitter-context
+      --    - Treesitter + textobjects: https://github.com/nvim-treesitter/nvim-treesitter-textobjects
     end,
   },
 
